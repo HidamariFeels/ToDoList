3b6426df6be1b11076d0ee453bfd98f0306cbf8f
HidamariFeels <207386338+HidamariFeels@users.noreply.github.com>
2025-06-11 00:30:40 +0100

    Added core command line functionality, can add/check/show tasks through the terminal

--- src/main/java/com/todolist/model/Main.java
+++ src/main/java/com/todolist/model/Main.java
@@ -9,24 +9,51 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     ToDoList todo = new ToDoList();
 
-    if (args.length == 1) {
+    while (true) {
+      System.out.print("Enter command: ");
+      String cmd = scanner.nextLine();
       
-      String arg = args[0];
+      if (cmd.equals("exit")) {
+        System.out.println("Exiting...");
+        break;
+      }
 
-      switch (arg) {
+      switch (cmd) {
         
         case "add":
-          System.out.println("Enter task:");
-          String taskInput = scanner.nextLine();
+          System.out.print("Enter task: ");
+          String addInput = scanner.nextLine();
 
-          todo.addTask(taskInput);
+          todo.addTask(addInput);
 
           System.out.println("Task added!");
+          break;
 
+        case "todo":
           todo.showToDoTasks();
+          break;
+          
+        case "done":
+          todo.showCompletedTasks();
+          break;
+
+        case "check":
+          System.out.println("Which task? (ID): ");
+          int checkInput = scanner.nextInt();
+          scanner.nextLine();
+          
+          try {
+          todo.completeTask(checkInput);
+        } catch (Exception e) {
+          System.out.println(e.getMessage());
+        }
+
+        break;
+
+        default:
+          System.out.println("Unknown command");
 
       }
     }
   }
-
 }
